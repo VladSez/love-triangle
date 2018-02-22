ef8f63f7d5fbe033e3a49e0feae66e719a60b2d8
Влад Сазонов <vladsazon27@gmail.com>
2018-02-22 23:35:50 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
    let love = 0;
   for(let i = 0; i<preferences.length; i++){
      let a  = preferences[i];
-     let b  = preferences[(a-1)];
-     let c  = preferences[(b-1)];
+     let b  = preferences[a-1];
+     let c  = preferences[b-1];
     if(c == i+1){
       love++;
     }
